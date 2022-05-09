30a579874f955e306649b2cff4416ae29b1e32e2
Sharonanota <matakwamatakwa@gmail.com>
2022-05-09 07:45:26 +0300

    madlib

---
+++ main.py
@@ -0,0 +1,9 @@
+color = input("Enter a color")
+noun = input("Enter a noun")
+celebrity = input("Enter a celebrity")
+
+
+print("Python is" + color)
+print(noun + "is interesting")
+print("I love" +celebrity)
+
